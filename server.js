54569789df54f2a74280b794a00d07f13004a68c
Juliette Rapala <jrapala@gmail.com>
2018-01-29 13:39:36 -0600

    Adding Spotify authorization process to server-side.

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const mongoose = require("mongoose");
 const apiRoutes = require("./routes/apiRoutes");
+const request = require('request')
+const querystring = require('querystring')
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8888;
 const app = express();
 
 // Configure body parser for AJAX requests
@@ -27,6 +29,44 @@ mongoose.connect(
     useMongoClient: true
   }
 );
+
+// SPOTIFY AUTHORIZATION
+let redirect_uri = 
+  process.env.REDIRECT_URI || 
+  'http://localhost:8888/callback'
+
+app.get('/login', function(req, res) {
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: process.env.SPOTIFY_CLIENT_ID,
+      scope: 'user-read-private user-read-email',
+      redirect_uri
+    }))
+})
+
+app.get('/callback', function(req, res) {
+  let code = req.query.code || null
+  let authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri,
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(
+        process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
+      ).toString('base64'))
+    },
+    json: true
+  }
+  request.post(authOptions, function(error, response, body) {
+    var access_token = body.access_token
+    let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
+    res.redirect(uri + '?access_token=' + access_token)
+  })
+})
 
 // Send every request to the React app
 // Define any API routes before this runs
