6b9597abf7dad72fde1816017fcc31b1d2e2c7df
Mark Walker <markwalkernz@gmail.com>
2018-02-01 11:44:13 -0600

    changed playlist to table

--- client/src/pages/Playlist/Playlist.js
+++ client/src/pages/Playlist/Playlist.js
@@ -3,28 +3,33 @@ import API from "../../utils/API";
 
 // Material UI components
 import RaisedButton from 'material-ui/RaisedButton';
-import {List, ListItem} from 'material-ui/List';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
 
 // Material UI styles
-const styles = {
-  deleteButtonStyle: {
-    marginLeft: 20
-  }
-};
+const styles = {};
 
 class Playlist extends Component {
 
   // Initial state
+  // selected is the index of the savedTrack that is currently selected
   state = {
     savedTracks: [],
+    selected: [],
     trackName: { type: String, required: true },
     artist: "",
     album: "",
     trackID: "",
     trackURL: "",
     trackURI: "",
-    energy: 0,
-    valence: 0
+    valence: 0,
+    energy: 0
   };
 
 
@@ -40,11 +45,24 @@ class Playlist extends Component {
 
     // Load tracks from DB
     API.getTracks()
-      .then(res =>
-        this.setState({ savedTracks: res.data })
-      )
+      .then(res => {
+        this.setState({ savedTracks: res.data });
+        console.log(res.data);
+      })
       .catch(err => console.log(err));
   };
+
+  // Row selection
+  isSelected = (index) => {
+      return this.state.selected.indexOf(index) !== -1;
+    };
+
+  handleRowSelection = (selectedRows) => {
+    this.setState({
+      selected: selectedRows,
+    });
+  };
+
 
   handleDeleteTrack = id => {
 
@@ -66,25 +84,40 @@ class Playlist extends Component {
           </div>
           <div>
             {this.state.savedTracks.length ? (
-              <List>
-                {this.state.savedTracks.map(track => {
-                  return (
-                  <ListItem key={track._id}>
-                    <p><strong>{track.trackName}</strong></p>
-                    <p>Artist: {track.artist}</p>
-                    <p>Album: {track.album}</p>
-                    <p>Positivity: {track.valence}</p>
-                    <p>Energy: {track.energy}</p>
-                    <a rel="noreferrer noopener" href={track.trackURL} target="_blank">Go to track</a>
-                    <RaisedButton
-                      label="Delete"
-                      onClick={() => this.handleDeleteTrack(track._id)}
-                      style={styles.deleteButtonStyle}
-                    />
-                  </ ListItem>
-                  )
-                })}
-              </ List>
+              <Table onRowSelection={this.handleRowSelection}>
+               <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+                  <TableRow>
+                    <TableHeaderColumn>Title</TableHeaderColumn>
+                    <TableHeaderColumn>Artist</TableHeaderColumn>
+                    <TableHeaderColumn>Album</TableHeaderColumn>
+                    <TableHeaderColumn>Positivity</TableHeaderColumn>
+                    <TableHeaderColumn>Energy</TableHeaderColumn>
+                    <TableHeaderColumn>Lyrics</TableHeaderColumn>
+                    <TableHeaderColumn>Button</TableHeaderColumn>
+                  </TableRow>
+                </TableHeader>
+                <TableBody displayRowCheckbox={false}>
+                  {this.state.savedTracks.map((track, index) => {
+                    return (
+                    <TableRow key={track._id}  selected={this.isSelected(index)}>
+                      <TableRowColumn>{track.trackName}</TableRowColumn>
+                      <TableRowColumn>{track.artist}</TableRowColumn>
+                      <TableRowColumn>{track.album}</TableRowColumn>
+                      <TableRowColumn>{track.valence}</TableRowColumn>
+                      <TableRowColumn>{track.energy}</TableRowColumn>
+                      <TableRowColumn>Lyrics</TableRowColumn>
+                      <TableRowColumn>
+                        <RaisedButton
+                          label="Delete"
+                          onClick={() => this.handleDeleteTrack(track._id)}
+                          style={styles.deleteButtonStyle}
+                        />
+                      </TableRowColumn>
+                    </ TableRow>
+                    )
+                  })}
+                </TableBody>
+              </ Table>
             ) : (<h1>No tracks in this playlist.</h1>)}
           </div>
         </div>
