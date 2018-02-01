e1d03ca70f21a4917f3359b568fd80221fdb0f8b
victoriapalacios <victoria@launchpadlab.com>
2018-02-01 12:39:29 -0600

    front end styles

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -7,12 +7,18 @@ import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import {List, ListItem} from 'material-ui/List';
 import {orange500, blue500} from 'material-ui/styles/colors';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
 
 // Material UI styles
-const styles = {
-  saveButtonStyle: {
-    marginLeft: 20
-  }
+const style = {
+
 };
 
 class Home extends Component {
@@ -29,7 +35,7 @@ class Home extends Component {
   }
 
 
-  // loadSpotifyUserData() {  
+  // loadSpotifyUserData() {
   // }
 
   searchSpotify(query) {
@@ -59,7 +65,7 @@ class Home extends Component {
     };
 
     console.log(request_params);
-    
+
 
     fetch(FETCH_URL, request_params)
       .then(response => response.json())
@@ -129,55 +135,73 @@ class Home extends Component {
           {/*<h2>Hello {this.state.userData.user.name}</h2>*/}
           <div>
             <div>
-              <h2>Search for a song</h2>
-            </div>
-            <div>
-              <TextField
-                hintText="Enter Artist, Track Name etc..."
-                name="query"
-                value={this.state.query}
-                onChange={this.handleInputChange}
+            <div><img src={'https://s10.postimg.org/hvq64sq1l/search-background.jpg'} alt="search" style={{ height: '200', width: '100%' }}/></div>
 
-              />
-              <br />
-              <RaisedButton
-                label="Search"
-                onClick={this.handleFormSubmit}
-                primary={true}
-              />
+              <div style={{margin: '0 auto', display: 'block', textAlign: 'center', marginTop: -112, marginBottom: 120}}>
+                <TextField
+                  hintText="Enter Artist, Track Name etc..."
+                  name="query"
+                  value={this.state.query}
+                  onChange={this.handleInputChange}
+                  style={{backgroundColor: '#FFFFFF', display: 'inline-block', width: 600}}
+                />
+
+                <RaisedButton
+                  label="Search"
+                  onClick={this.handleFormSubmit}
+                  primary={true}
+                  style={{backgroundColor: '#5A66E3', borderRadius: 0, border: 0, height: 48, padding: '0 30px', display: 'inline-block',}}
+                />
+              </div>
+
             </div>
           </div>
           <div>
             <div>
-              <h2>Results</h2>
+
             </div>
             <div>
               {this.state.tracks.length ? (
                 <List>
                   {this.state.tracks.map(track => {
                     return (
-                    <ListItem>
-                      <p><strong>{track.trackName}</strong></p>
-                      <p>Artist: {track.artist}</p>
-                      <p>Album: {track.album}</p>
-                      <a rel="noreferrer noopener" href="track.trackURL" target="_blank">Go to track</a>
-                      <RaisedButton
-                        label="Add to Playlist"
-                        onClick={() => this.handleSaveTrack(track)}
-                        style={styles.saveButtonStyle}
-                        />
-                    </ ListItem>
+                    <Table style={{ maxWidth: 1000, margin: '0 auto', backgroundColor: '#F7F9FF', padding: 20}}>
+                      <TableHeader>
+    {/*                    <TableRow>
+                          <TableHeaderColumn>Song</TableHeaderColumn>
+                          <TableHeaderColumn>Artist</TableHeaderColumn>
+                          <TableHeaderColumn>Album</TableHeaderColumn>
+                        </TableRow>*/}
+                      </TableHeader>
+                      <TableBody style={{padding: 10, display: 'inlineBlock', fontFamily: 'Montserrat'}}>
+                        <TableRow>
+                          <TableRowColumn>{track.trackName}</TableRowColumn>
+                          <TableRowColumn>{track.artist}</TableRowColumn>
+                          <TableRowColumn>{track.album}</TableRowColumn>
+                          <TableRowColumn><a rel="noreferrer noopener" href="track.trackURL" target="_blank"></a>
+                            <RaisedButton
+                              label="Add"
+                              onClick={() => this.handleSaveTrack(track)}
+                              style={{textTransform: 'uppercase', border: '1px solid #5A66E3', borderRadius: 0, boxShadow: 'none', height: 'initial', padding: '10px', backgroundColor: 'transaprent'}}
+                              />
+                          </TableRowColumn>
+                        </TableRow>
+                      </TableBody>
+                    </ Table>
                     )
                   })}
                 </ List>
-              ) : (<h1>No tracks, try a new search!</h1>)}
+              ) : (<div style={{ margin: '0 auto', maxWidth: 500, textAlign: 'center', marginTop: 120}}>
+                <img style={{width: 150, marginTop: 50}} src='https://s17.postimg.org/vobidfu3z/start-searaching.png' alt="Start Searching" />
+                <h2 style={{ fontWeignt: 100, fontFamily: 'Montserrat'}}>Start by searching for a song. Then click “Add” to begin curating your playlist.</h2>
+              </div>)}
             </div>
           </div>
           </div>
         {/*}) : (
           <button onClick={ () => window.location='https://accounts.spotify.com/authorize?client_id=6f49983391014a5a99a289c59c92d0af&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fcallback&scope=user-read-private%20user-read-email&response_type=token&state=3125606776'} style={{padding:'20px', 'font-size':'50px', 'margin-top':'20px'}}>Click here to login to Spotify</button>
         )*/}
-      
+
 
       </div>
     )
@@ -185,3 +209,4 @@ class Home extends Component {
 }
 
 export default Home;
+
--- client/src/pages/LoginPage/LoginPage.js
+++ client/src/pages/LoginPage/LoginPage.js
@@ -4,8 +4,11 @@ import React, { Component } from "react";
 class LoginPage extends React.Component {
 	render() {
 		return (
-			<div>
-				<button onClick={ () => window.location='https://accounts.spotify.com/authorize?client_id=6f49983391014a5a99a289c59c92d0af&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fhome%2Fcallback&scope=user-read-private%20user-read-email&response_type=token&state=3125606776'} style={{padding:'20px', 'font-size':'50px', 'margin-top':'20px'}}>Click here to login to Spotify</button>
+			<div style={{ backgroundImage: 'url(https://s17.postimg.org/z057sw1lb/landing-background.jpg)', backgroundPosition: 'center', backgorundRepeat: 'no-repeat', backgroundSize: 'cover', position: 'fixed', height: '100%', width: '100%'}}>
+
+        <h1 style={{padding:'20px 40px', 'font-size':'50px', margin:'0 auto', maxWidth: 900, textAlign: 'center', display: 'block', color: '#FFFFFF', letterSpacing: 1, paddingTop: 300}}>search music and curate your own playlist based on your mood</h1>
+
+        <button onClick={ () => window.location='https://accounts.spotify.com/authorize?client_id=6f49983391014a5a99a289c59c92d0af&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fhome%2Fcallback&scope=user-read-private%20user-read-email&response_type=token&state=3125606776'} style={{padding:'20px 50px', 'font-size':'16px', margin:'0 auto', textAlign: 'center', display: 'block', textTransform: 'uppercase', backgroundColor: '#5A66E3', color: '#FFFFFF', fontWeight: 'bold', letterSpacing: 2, border: 0, cursor: 'pointer', marginTop: 20}}>Sign Up</button>
 			</div>
 		)
 	};
