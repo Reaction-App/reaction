7a46ee4f721c9a3bca8e9e2d3c1e561bf61a2c95
Juliette Rapala <jrapala@gmail.com>
2018-01-31 20:37:17 -0600

    Changing redirect back to local.

--- client/src/pages/LoginPage/LoginPage.js
+++ client/src/pages/LoginPage/LoginPage.js
@@ -11,8 +11,12 @@ import React, { Component } from "react";
 
 	// Heroku: REDIRECT_URL set to 'https://desolate-caverns-55074.herokuapp.com/home/callback'
 
+
+	// Local Redirect
+	const REDIRECT_URI = encodeURIComponent('http://localhost:3000/home/callback');
+
 	// Heroku Redirect
-	const REDIRECT_URI = encodeURIComponent('https://desolate-caverns-55074.herokuapp.com/home/callback');
+	// const REDIRECT_URI = encodeURIComponent('https://desolate-caverns-55074.herokuapp.com/home/callback');
 
 	// Spotify scopes: https://developer.spotify.com/web-api/using-scopes/
 
