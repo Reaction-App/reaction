feb92aefe0aae460f2d3a9b8a982443951a74545
Juliette Rapala <jrapala@gmail.com>
2018-02-15 19:29:36 -0600

    Fixing merge error.

--- client/src/pages/Playlist/Playlist.js
+++ client/src/pages/Playlist/Playlist.js
@@ -70,7 +70,12 @@ const Playlist = props => {
               </div>
             </div>
           ) : (<div></div>)}
+    </div>
+
+
+
     <div className="playlist-container">
+
       <div>
         <DropDownMenu value={props.sortDropDown} onChange={props.handlePlaylistSort} style={{ width: '50%'}}>
           <MenuItem value={0} primaryText="Sort by" />
@@ -93,34 +98,30 @@ const Playlist = props => {
         </DropDownMenu>
       </div>
       <div className="playlist-container">
-  
-      <h2 className="playlist-header" style={{color: 'white', backgroundColor: '#5A66E3', padding: 10, fontFamily: 'Montserrat'}}>My Playlist <a>Export To Spotify</a></h2>
-
-      {props.savedTracks.length ? (
-        <List className="list" style={{backgroundColor: '#F7F9FF', border: '1px solid #5A66E3', marginTop: '-20px', maxHeight: 518, overflow: 'scroll', float: 'left'}}>
-
-          {props.savedTracks.map((track, index) => {
-            return (
-              <ListItem
-              className="meep"
-                style={{paddingBottom: 0}}
-                id="songItem"
-                key={track._id}
-                leftIcon={
-                  <IconButton
-                style={{top: -8}}
-                disabled={track.trackURL === null ? true : false}
-                tooltip={track.trackURL === null ? 'Not Available' : false}
-                onClick={() => props.playTrack(track.trackURL, track.trackID)}
-              >
-                <FontIcon className="material-icons">
-                {props.currentSongPlayingID === track.trackID && props.songPlaying === true ? "play_circle_filled" : "play_circle_outline"}
-                </FontIcon>
-              </IconButton>
-                }
-                onClick={() => props.highlightThis(track.trackID)}
-              >
-
+        <h2 className="playlist-header" style={{color: 'white', backgroundColor: '#5A66E3', padding: 10, fontFamily: 'Montserrat'}}>My Playlist <a>Export To Spotify</a></h2>
+        {props.savedTracks.length ? (
+          <List className="list" style={{backgroundColor: '#F7F9FF', border: '1px solid #5A66E3', marginTop: '-20px', maxHeight: 518, overflow: 'scroll', float: 'left'}}>
+            {props.savedTracks.map((track, index) => {
+              return (
+                <ListItem
+                  className="meep"
+                  style={{paddingBottom: 0}}
+                  id="songItem"
+                  key={track._id}
+                  leftIcon={
+                    <IconButton
+                      style={{top: -8}}
+                      disabled={track.trackURL === null ? true : false}
+                      tooltip={track.trackURL === null ? 'Not Available' : false}
+                      onClick={() => props.playTrack(track.trackURL, track.trackID)}
+                    >
+                      <FontIcon className="material-icons">
+                      {props.currentSongPlayingID === track.trackID && props.songPlaying === true ? "play_circle_filled" : "play_circle_outline"}
+                      </FontIcon>
+                    </IconButton>
+                  }
+                  onClick={() => props.highlightThis(track.trackID)}
+                >
               <div className="action-icons">
                 <img className="sort" onClick={() => props.handleSortBySelected(index)} src="https://s14.postimg.org/f0aj9ige9/sort.png" alt="Sort" />
 
@@ -137,16 +138,18 @@ const Playlist = props => {
                   <p style={{marginTop: 0, marginBottom: 0, paddingBottom: 5, fontFamily: 'Montserrat', fontSize: 12, maxWidth: 280, whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', display: 'inline-block'}}>
                     Positivity: {track.valence}% | Energy: {track.energy}%
                   </p>
-                  <div style={{display: 'inline-block', marginLeft: 10, marginTop: -15, verticalAlign: 'middle'}}>{props.showEmotion(track.valence,track.energy)}</div>
+                  <div style={{display: 'inline-block', marginLeft: 10, marginTop: -15, verticalAlign: 'middle'}}>{props.showEmotion(track.valence,track.energy)}
+                  </div>
                 </div>
 
               </ListItem>
-            )})
-        }
+            )}
+            )}
         </List>
       ) : (<p style={{backgroundColor: '#F7F9FF', border: '1px solid #5A66E3', marginTop: '-20px', width: 380, maxHeight: 518, overflow: 'scroll', float: 'left', padding: 20}}>Once you start adding songs, they will show up here in your playlist.</p>)}
-
+      </div>
     </div>
-    )}
-
+    </div> 
+  )
+}
 export default Playlist;
